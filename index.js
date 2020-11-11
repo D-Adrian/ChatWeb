5333e84b5293bdfb124d1f06c2a9c79181596098
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2020-11-11 11:15:30 +0100

    not fonctional connection refused but set headers

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const { initExpressApp, hookWithServer } = require('./webserver')
+http = require('http');
 
-const app = initExpressApp()
-const server = hookWithServer(app)
-const  router  =  express.Router();
-
-
-router.route("/").get((req, res, next) =>  {
-    res.setHeader("Content-Type", "application/json");
-    res.statusCode  =  200;
-    connectdb.then(db  =>  {
-        Chats.find({}).then(chat  =>  {
-        res.json(chat);
-    });
-});
-
-server.listen(4021, () => {
-    console.log('Listening on http://localhost:4021')
+//const app = initExpressApp()
+var app = express();
+app.use(function(res, req, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 })
 
+const server = hookWithServer(app)
+
+// const httpHandler = function(req, res) {
+//     if (req.url === '/'){
+//         res.setHeader('Access-Control-Allow-Origin', '*')
+//         getIndex(req, res)
+//     }
+//     console.log('Listening on http://localhost:4021')
+//     res.end()
+// }
+
+app.get('/', function(req,res) {
+    res.sendFile('index.html');
+})
+
+server.listen(4021, (req, res) => {
+    console.log('Listening on http://localhost:4021')
+})
+//http.createServer(httpHandler).listen(4021)
 
